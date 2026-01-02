83577939ffef1f4808c21d43753bc1b8d9da6025
devback90 <devback90.nexsol@gmail.com>
2026-01-02 19:43:19 +0900

    feat: 문자 메일 추가

--- client/client-aligo/src/main/java/com/nexsol/tpa/client/aligo/AligoFeignConfig.java
+++ client/client-aligo/src/main/java/com/nexsol/tpa/client/aligo/AligoFeignConfig.java
@@ -1,19 +1,46 @@
 package com.nexsol.tpa.client.aligo;
 
+import feign.Logger;
+import feign.codec.Decoder;
 import feign.codec.Encoder;
 import feign.form.spring.SpringFormEncoder;
-import org.springframework.beans.factory.ObjectFactory;
-
 import org.springframework.beans.factory.ObjectProvider;
+
+import org.springframework.boot.http.converter.autoconfigure.ClientHttpMessageConvertersCustomizer;
 import org.springframework.cloud.openfeign.support.FeignHttpMessageConverters;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverters;
+import org.springframework.http.converter.json.JacksonJsonHttpMessageConverter;
+
+
+import java.util.List;
 
 public class AligoFeignConfig {
 
     @Bean
     public Encoder feignFormEncoder(ObjectProvider<FeignHttpMessageConverters> converters) {
         return new SpringFormEncoder(new SpringEncoder(converters));
+    }
+
+    @Bean
+    public ClientHttpMessageConvertersCustomizer aligoConverterCustomizer() {
+        return converters -> {
+            // Jackson 3용 컨버터 생성
+            JacksonJsonHttpMessageConverter converter = new JacksonJsonHttpMessageConverter();
+
+            // "text/html" 응답도 JSON으로 처리하도록 미디어 타입 추가
+            converter.setSupportedMediaTypes(List.of(MediaType.APPLICATION_JSON, MediaType.TEXT_HTML));
+
+            // 우선순위를 높이기 위해 리스트 맨 앞에 추가
+            converters.build();
+        };
+    }
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
